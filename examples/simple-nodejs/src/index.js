db51975afe5b99a35e89558f40a2650872af7729
Hao Luo <howlowck@gmail.com>
2023-02-20 23:11:53 -0600

    minor edits

--- examples/simple-nodejs/src/index.js
+++ examples/simple-nodejs/src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// UNGEN: Replace "World" with var.appName
+// UNGEN: replace "World" with kebabCase(var.appName)
 app.get('/', (req, res) => res.send('Hello World!'));
 
 // start the Express server
